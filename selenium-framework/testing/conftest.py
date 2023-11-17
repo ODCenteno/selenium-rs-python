68915b5481b3c90c2a6968eca7f5e7fc6aba2da3
= <pixie_cemodan@hotmail.com>
2023-11-16 23:47:20 -0700

    base code to CLI browser options

--- selenium-framework/testing/conftest.py
+++ selenium-framework/testing/conftest.py
@@ -1,14 +1,37 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chrome",
+        help="Select browser in CLI with: --browser  (default)",
+        choices=("firefox", "safari", "chrome")
+    )
 
 
 @pytest.fixture(scope="class")
 def setup(request):
-    service = Service()
-    driver = webdriver.Chrome(service=service)
+    # Obteniendo el parámetro del CLI para el navegador elegido
+    browser = request.config.getoption("--browser")
+
+    if browser == "chrome":
+        service = ChromeService()
+        driver = webdriver.Chrome(service=service)
+    elif browser == "firefox":
+        service = FirefoxService()
+        driver = webdriver.Firefox(service=service)
+    elif browser == "safari":
+        driver = webdriver.Safari()
+    else:
+        raise ValueError(f'Browser {browser} not supported.')
+
     driver.get("http://www.google.com")
     driver.maximize_window()
+
 
     # Asignamos el driver a la instancia request para utilizarlo en las Test classes
     # Una instancia request se genera automáticamente con pytest.fixture
